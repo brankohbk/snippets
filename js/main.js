f848f83ee474e4b4d31db6c92d07b49d74d93ba0
brankohbk <brankoh@gmail.com>
2020-02-23 01:24:19 -0300

    Se añadieron 3 cards para probar el diseño. Próximo paso: implementar componentes de Vue

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// Fixes unnecessary scrolling in mobile
+let vh, vw;
+function updateSize() {
+  vh = window.innerHeight * 0.01;
+  vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+}
+window.addEventListener("resize", updateSize)
+updateSize();
+
+
